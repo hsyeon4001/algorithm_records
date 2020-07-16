c0e84ab4243fc8624ded41929741fc9aee3bbba6
hsyeon4001 <hsyeon4001@gmail.com>
2020-07-16 18:58:04 +0900

    second commit of leetcode

--- Python/leetcode/0711_0717.py
+++ Python/leetcode/0711_0717.py
@@ -1,4 +1,4 @@
-# 1493
+# 1493(array)
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         arr = []
@@ -14,3 +14,43 @@ class Solution:
             answer = max(answer, right-left-2)
 
         return answer
+
+# 1418(hash table)
+class Solution:
+    def displayTable(self, orders: List[List[str]]) -> List[List[str]]:
+        table_dict, order_table = {}, []
+        display_table = [[]]
+
+        for order in orders:
+            if order[2] not in display_table[0]:
+                display_table[0].append(order[2])
+                
+            if order[1] in table_dict:
+                if order[2] in table_dict[order[1]]:
+                    table_dict[order[1]][order[2]] += 1
+                else:
+                    table_dict[order[1]][order[2]] = 1
+            else:
+                table_dict[order[1]] = {order[2]: 1}
+
+        display_table[0].sort()
+        d = display_table.pop()
+
+        for table, order in table_dict.items():
+            for menu in d:
+                if menu in order:
+                    order_table.append(str(order[menu]))
+                else:
+                    order_table.append('0')
+            order_table.insert(0, int(table))
+            display_table.append(order_table)
+            order_table = []
+        display_table.sort()
+        
+        for display in display_table:
+            display[0] = str(display[0])
+
+        display_table.insert(0, d)
+        display_table[0].insert(0, 'Table')
+        
+        return display_table
